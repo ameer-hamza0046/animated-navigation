448ef300ad93f74732a0a4778b9153def1f20e20
Ameer Hamza <ameer.hamza0046@gmail.com>
2023-07-05 18:24:07 +0530

    functionality of toggling the navbar added

---
+++ index.js
@@ -0,0 +1,6 @@
+const navbar = document.querySelector(".navbar");
+const navToggle = document.querySelector(".nav-toggle")
+
+navToggle.addEventListener("click", () => {
+    navbar.classList.toggle("hidden")
+})
